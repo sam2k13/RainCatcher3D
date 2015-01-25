2b4bf9dd1debd0e469b36970e63c158f5ca19d15
sam2k13 <sam2k13@gmail.com>
2015-01-24 22:42:40 -0600

    Added Rain Drop

---
+++ Assets/GameController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public float minZ,maxZ,minX,MaxX;
+
+	void Start () {
+	
+	}
+	
+
+	void Update () {
+	
+	}
+}
